e5509e97cb4f5c9923f6548e1c45425a7996f42f
V-Mayya <87191173+V-Mayya@users.noreply.github.com>
2023-04-19 22:31:18 +0800

    Update challenge_code.py

--- challenge_code.py
+++ challenge_code.py
@@ -1,6 +1,6 @@
-##Week 16 challenge - Data in Motion
-#Create a function that takes a single string as argument and returns an ordered list containing the indices of all capital letters in the string.
-#e.g: index_of_caps("eDaBiT") ➞ [1, 3, 5] 
+## Week 16 challenge - Data in Motion
+# Create a function that takes a single string as argument and returns an ordered list containing the indices of all capital letters in the string.
+# e.g: index_of_caps("eDaBiT") ➞ [1, 3, 5] 
 
 def input(string):
     string_list = []
@@ -17,11 +17,11 @@ def input(string):
 
 input("HeLLo!")
 
-#output: [0, 2, 3] 
+# output: [0, 2, 3] 
 
-##A slight variation of the 'Crazy Kangaroo' Challenge on Hackerearth (https://www.hackerearth.com/problem/algorithm/crazy-kangaroo):
-#Goal: For each test case print the number of jumps the kangaroo had to make in the range [A, B] inclusive. 
-#Note: The kangaroo jumps only to those positions which are multiples of M or hop count.
+## A slight variation of the 'Crazy Kangaroo' Challenge on Hackerearth (https://www.hackerearth.com/problem/algorithm/crazy-kangaroo):
+# Goal: For each test case print the number of jumps the kangaroo had to make in the range [A, B] inclusive. 
+# Note: The kangaroo jumps only to those positions which are multiples of M or hop count.
 
 list = [5, 10, 3] #input (A and B are integers (start and end points are inclusive), M = hop count)
 
@@ -45,8 +45,8 @@ count_of_multiple_values = len(multiple_values)
 
 print(count_of_multiple_values) #output in this case is 2
 
-##Week 17 challenge - Data in Motion
-#1. Given a list ["Elie", "Tim", "Matt"], create a variable called answer, which is a new list containing the first letter of each name in the list. 
+## Week 17 challenge - Data in Motion
+# 1. Given a list ["Elie", "Tim", "Matt"], create a variable called answer, which is a new list containing the first letter of each name in the list. 
 
 names_list = ["Ellie", "Tim", "Matt"]
 answer = []
@@ -57,7 +57,7 @@ for name in names_list:
     answer.append(first_letter)
 print(answer)
 
-#2. Given a list [1,2,3,4,5,6], create a new variable called answer2, which is a new list of all the even values. 
+# 2. Given a list [1,2,3,4,5,6], create a new variable called answer2, which is a new list of all the even values. 
 
 list = [1, 2, 3, 4, 5, 6]
 answer2 = []
@@ -67,9 +67,9 @@ for num in list:
         answer2.append(num)     
 print(answer2)
 
-#output: [2, 4, 6] 
+# output: [2, 4, 6] 
 
-#extension: Given a list [2,4,7,3,14,19], check if a number in the list is odd (output: 'True'). Otherwise (even number): 'False'. 
+# extension: Given a list [2,4,7,3,14,19], check if a number in the list is odd (output: 'True'). Otherwise (even number): 'False'. 
 
 l = [2,4,7,3,14,19]
 for i in l:
@@ -77,10 +77,10 @@ for i in l:
     odd_check = lambda i: True if number%2 != 0 else False
     print(odd_check(i))  
   
-#output: False, False, True, True, False, True    
+# output: False, False, True, True, False, True    
     
-##Week 19 challenge - Data in Motion
-#Given a list of client emails, create a function that takes in the list as an argument and returns a new list with only the domain of each email. 
+## Week 19 challenge - Data in Motion
+# Given a list of client emails, create a function that takes in the list as an argument and returns a new list with only the domain of each email. 
 # e.g: clients = ['brucewayne@gotham.com', 'homer_simpson@springfieldnuclear.com', 'hank_hill@arlenpropane.com', 'petergriffin@pawtucketbrewery.com']
 # output: get_domains(clients) = ['gotham.com', 'springfieldnuclear.com', 'arlenpropane.com', 'pawtucketbrewery.com']
 
@@ -95,10 +95,10 @@ def get_domains(clients):
 
 print(get_domains(clients))
 
-##Week 23 challenge - Data in Motion
-#Define a function contains_blue() that accepts any number of arguments. It should return True if any of the arguments are "blue" (all lowercase). 
-#Otherwise, it should return False. 
-#contains_blue("green", False, 37, "purple", "hello world") -> output: False
+## Week 23 challenge - Data in Motion
+# Define a function contains_blue() that accepts any number of arguments. It should return True if any of the arguments are "blue" (all lowercase). 
+# Otherwise, it should return False. 
+# contains_blue("green", False, 37, "purple", "hello world") -> output: False
 
 def contains_blue(*input):
     true_false_list = [input[i]=="blue" for i in range(len(input))]
@@ -106,13 +106,13 @@ def contains_blue(*input):
     
 print(contains_blue("green", False, 37, "purple", "hello world"))
 
-#OR alternative solution (after def contains_blue(*input)):
+# OR alternative solution (after def contains_blue(*input)):
 true_false_list = [i=="blue" for i in input]
 
-##Week 24 challenge - Data in Motion
-#Given a person variable: person = [["name", "Bruce"], ["job", "Batman"], ["city", "Gotham"]], 
-#Create a dictionary called answer , that makes each first item in each list a key and the second item a corresponding value. 
-#This is the end goal: {'name': 'Bruce', 'job': 'Batman', 'city': 'Gotham'}.
+## Week 24 challenge - Data in Motion
+# Given a person variable: person = [["name", "Bruce"], ["job", "Batman"], ["city", "Gotham"]], 
+# Create a dictionary called answer , that makes each first item in each list a key and the second item a corresponding value. 
+# This is the end goal: {'name': 'Bruce', 'job': 'Batman', 'city': 'Gotham'}.
 
 person = [["name", "Bruce"], ["job", "Batman"], ["city", "Gotham"]]
 answer = {}
@@ -121,20 +121,20 @@ for list in person:
 
 print(answer)
 
-##Week 26 challenge - Data in Motion
-#Optional bonus: Program that simulates rock, paper, scissors: 
-#(Elementary game where first to 3 points wins)
+## Week 26 challenge - Data in Motion
+# Optional bonus: Program that simulates rock, paper, scissors: 
+# (Elementary game where first to 3 points wins)
 import random
 
-#Points dictionary (R beats S and P, S beats P and P beats none - slight variation):
+# Points dictionary (R beats S and P, S beats P and P beats none - slight variation):
 points = {"Rock": 2, "Scissors": 1,"Paper": 0}
 
-#Computer choice 
+# Computer choice 
 comp_choice_list = ["Rock", "Scissors", "Paper"]
 new_comp_list_random = [comp_choice_list[random.randint(0,2)] for choice in comp_choice_list]
 new_comp_list_points = [points[newchoice] for newchoice in new_comp_list_random]
 
-#User choice
+# User choice
 n = 0
 user_choice_list = []
 for n in range(3):
@@ -145,7 +145,7 @@ for n in range(3):
 
 user_list_points = [points[userchoice] for userchoice in user_choice_list]
 
-#Computer (c_list) and user points (u_list): 
+# Computer (c_list) and user points (u_list): 
 u_list = []
 c_list = []
     
@@ -158,10 +158,49 @@ for user, comp in zip(user_list_points, new_comp_list_points):
         u_points = 1
         u_list.append(u_points)
 
-#Comparing points: 
+# Comparing points: 
 if sum(u_list) > sum(c_list):
     print("Great, you win! You got {} points while I got {}.".format((sum(u_list)),(sum(c_list))))
 elif sum(u_list) == sum(c_list):
     print("Sorry, it's a tie. You got {} points while I got {}.".format((sum(u_list)),(sum(c_list))))
 else:
     print("Sorry, I win. You got {} points while I got {}.".format((sum(u_list)),(sum(c_list))))
+ 
+## Week 35 challenge - Data in Motion
+# Hard challenge: Create a function that returns the majority vote in a list. 
+# A majority vote is an element that occurs > N/2 times in a list (where N is the length of the list). 
+# E.g: majority_vote(["A", "A", "A", "B", "C", "A"]) ➞ "A". majority_vote(["A", "B", "B", "A", "C", "C"]) ➞ None
+
+def majority_vote(list_of_votes):
+    
+    # distinct set of votes in list
+    list_of_votes_copy = []
+    
+    for vote in list_of_votes:
+        if vote not in list_of_votes_copy:
+            list_of_votes_copy.append(vote)
+        
+    # check if distinct votes in copy matches original list of votes and counting number of votes for each distinct vote
+    count_list = []
+    
+    for vote in list_of_votes_copy:
+        n = 0
+        for i in range(len(list_of_votes)): 
+            if vote == list_of_votes[i]:
+                n = n+1
+        tuple_values = (n, vote)
+        count_list.append(tuple_values) 
+        
+    for number_of_votes, distinct_vote in count_list:
+        if number_of_votes > len(list_of_votes)/2:
+            message = f"{distinct_vote}"
+            break
+        # either no majority vote or empty list
+        else:
+            message = "None"
+    return message
+
+majority_votes = majority_vote(["A", "B", "B", "A", "C", "C"])
+print(majority_votes)  
+ 
+
